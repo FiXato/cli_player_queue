210fa28d9b1da281953e3bafafecd5ba838e2859
Filip H.F. "FiXato" Slagter <fixato+github+git@gmail.com>
2021-01-12 00:24:31 +0100

    Fix merging feeds, add ensure_param better feed menu
    also allow setting channel id in the user's youtube config dir for later use.

--- functions.sh
+++ functions.sh
@@ -65,20 +65,37 @@ function curl_twitch {
 	curl --location --silent --show-error -H 'Accept: application/vnd.twitchtv.v5+json' -H "Client-ID: ${TWITCH_CLIENT_ID}" "$@"
 }
 
+function ensure_param {
+	local _param_nr="${1:-""}"
+	local _index=$(( $_param_nr + 2))
+	local _param="${!_index}"
+	local _desc="${2:-""}"
+
+	[[ "$_param" != "" ]] && printf '%s' "$_param" || (error_msg $ERROR_MISSING_PARAMETER_NO "$ERROR_MISSING_PARAMETER_MSG \$$_param_nr ($_desc)"; exit $ERROR_MISSING_PARAMETER_NO)
+}
+
+function test_ensure_param {
+	param_value="$(ensure_param 4 "param description" "$@")" || exit $?
+	echo "param value: $param_value"
+}
+
+function store_channel_id_for_user {
+	youtube_username="$(ensure_param 1 "youtube_username" "$@")" || exit $?
+	youtube_channel_id="$(ensure_param 2 "youtube_channel_id" "$@")" || exit $?
+
+	youtube_user_dir="$(youtube_dir_for_user "$youtube_username")" || exit $?
+	channel_id_file="$youtube_user_dir/channel_id.txt"
+	printf '%s' "$youtube_channel_id" > "$channel_id_file"
+}
+
 function channel_id_for_youtube_username {
 	youtube_username="${1:-""}"
 	[[ "$youtube_username" == "" ]] && error_msg $ERROR_MISSING_PARAMETER_NO "$ERROR_MISSING_PARAMETER_MSG \$1 (youtube_username)" && exit $ERROR_MISSING_PARAMETER_NO
 
-	if [ "$youtube_username" == "AndroidGamingFiX" ]; then
-		printf '%s' "UC3A5hOWtXCXX3UYN2SV9R0A"
-	elif [ "$youtube_username" == "ChefBuck" ]; then
-		printf '%s' "UCAVgKTAg-h8jlMTPKgKsXSQ"
-	elif [ "$youtube_username" == "extracredits" ]; then
-		printf '%s' "UCCODtTcd5M1JavPCOr_Uydg"
-	elif [ "$youtube_username" == "TechnologyConnections" ]; then
-		printf '%s' "UCy0tKL1T7wFoYcxCe0xjN6Q"
-	elif [ "$youtube_username" == "TechnologyConnextras" ]; then
-		printf '%s' "UClRwC5Vc8HrB6vGx6Ti-lhA"
+	youtube_user_dir="$(youtube_dir_for_user "$youtube_username")" || exit $?
+	channel_id_file="$youtube_user_dir/channel_id.txt"
+	if [ -f "$channel_id_file" ]; then
+		cat "$channel_id_file"
 	else
 		curl --silent "$INVIDIOUS_INSTANCE_URL/channel/$youtube_username" | pup 'input[name="q"] attr{value}' | grep -oP 'channel:\K(\S+)'
 	fi
@@ -103,16 +120,16 @@ function youtube_dir_for_user {
 	[[ "$youtube_username" == "" ]] && error_msg $ERROR_MISSING_PARAMETER_NO "$ERROR_MISSING_PARAMETER_MSG \$1 (youtube_username)" && exit $ERROR_MISSING_PARAMETER_NO
 
 	#FIXME: filename sanitise. For now it should be safe though, as I manually enter the usernames.
-	printf '%s' "$PLAY_VIDEO_YOUTUBE_DIR/$youtube_username"
+	youtube_user_dir="$(printf '%s' "$PLAY_VIDEO_YOUTUBE_DIR/$youtube_username")" || (error_msg $ERROR_GETTING_YOUTUBE_DIR_FOR_USER_NO "$ERROR_GETTING_YOUTUBE_DIR_FOR_USER_MSG $youtube_username ($?)" && exit $ERROR_GETTING_YOUTUBE_DIR_FOR_USER_NO)
+	[ ! -d "$youtube_user_dir" ] && (error_msg 0 "YouTube feeds dir for '$youtube_username' does not exist yet; trying to create" && mkdir -p "$youtube_user_dir" || (error_code=$? && error_msg $error_code "Error while creating YouTube feeds dir for '$youtube_username': '$youtube_user_dir'" && exit $error_code))
+	printf '%s' "$youtube_user_dir"
 }
 
 function youtube_new_videos_feed_file_for_user {
 	youtube_username="${1:-""}"
 	[[ "$youtube_username" == "" ]] && error_msg $ERROR_MISSING_PARAMETER_NO "$ERROR_MISSING_PARAMETER_MSG \$1 (youtube_username)" && exit $ERROR_MISSING_PARAMETER_NO
 
-	youtube_user_dir="$(youtube_dir_for_user "$youtube_username")" || (error_msg $ERROR_GETTING_YOUTUBE_DIR_FOR_USER_NO "$ERROR_GETTING_YOUTUBE_DIR_FOR_USER_MSG $youtube_username ($?)" && exit $ERROR_GETTING_YOUTUBE_DIR_FOR_USER_NO)
-	[ ! -d "$youtube_user_dir" ] && (error_msg 0 "YouTube feeds dir for '$youtube_username' does not exist yet; trying to create" && mkdir -p "$youtube_user_dir" || (error_code=$? && error_msg $error_code "Error while creating YouTube feeds dir for '$youtube_username': '$youtube_user_dir'" && exit $error_code))
-
+	youtube_user_dir="$(youtube_dir_for_user "$youtube_username")" || exit $?
 	printf '%s' "$youtube_user_dir/new.json"
 }
 
@@ -120,21 +137,24 @@ function youtube_videos_feed_file_for_user {
 	youtube_username="${1:-""}"
 	[[ "$youtube_username" == "" ]] && error_msg $ERROR_MISSING_PARAMETER_NO "$ERROR_MISSING_PARAMETER_MSG \$1 (youtube_username)" && exit $ERROR_MISSING_PARAMETER_NO
 
-	printf '%s' "$(youtube_dir_for_user "$youtube_username")/videos.json"
+	youtube_user_dir="$(youtube_dir_for_user "$youtube_username")" || exit $?
+	printf '%s' "$youtube_user_dir/videos.json"
 }
 
 function youtube_new_videos_titles_file_for_user {
 	youtube_username="${1:-""}"
 	[[ "$youtube_username" == "" ]] && error_msg $ERROR_MISSING_PARAMETER_NO "$ERROR_MISSING_PARAMETER_MSG \$1 (youtube_username)" && exit $ERROR_MISSING_PARAMETER_NO
 
-	printf '%s' "$(youtube_dir_for_user "$youtube_username")/new.txt"
+	youtube_user_dir="$(youtube_dir_for_user "$youtube_username")" || exit $?
+	printf '%s' "$youtube_user_dir/new.txt"
 }
 
 function youtube_videos_titles_file_for_user {
 	youtube_username="${1:-""}"
 	[[ "$youtube_username" == "" ]] && error_msg $ERROR_MISSING_PARAMETER_NO "$ERROR_MISSING_PARAMETER_MSG \$1 (youtube_username)" && exit $ERROR_MISSING_PARAMETER_NO
 
-	printf '%s' "$(youtube_dir_for_user "$youtube_username")/videos.txt"
+	youtube_user_dir="$(youtube_dir_for_user "$youtube_username")" || exit $?
+	printf '%s' "$youtube_user_dir/videos.txt"
 }
 
 function store_recent_youtube_feeds_for_user {
@@ -149,18 +169,23 @@ function store_recent_youtube_feeds_for_user {
 	feed_filename="$(youtube_videos_feed_file_for_user "$youtube_username")"
 	[ ! -f "$feed_filename" ] && echo '{}' > "$feed_filename" && error_msg 0 "'$feed_filename' did not exist, so an empty one has been created" >&2
 	temp_feed_filename="$(youtube_videos_feed_file_for_user "$youtube_username").temp"
-	merge_json_files "$new_feed_filename" "$feed_filename" > "$temp_feed_filename"
+	merge_youtube_feed_files "$new_feed_filename" "$feed_filename" > "$temp_feed_filename"
 	error_code=$?
 	(( $error_code > 0 )) && error_msg $ERROR_MERGING_YOUTUBE_VIDEO_FEEDS_FOR_USER_NO "$ERROR_MERGING_YOUTUBE_VIDEO_FEEDS_FOR_USER_MSG '$youtube_username' ($new_feed_filename + $feed_filename >  $temp_feed_filename) (process returned: $error_code)" && exit $ERROR_MERGING_YOUTUBE_VIDEO_FEEDS_FOR_USER_NO
 
 	mv "$temp_feed_filename" "$feed_filename"
 	error_code=$?
 	(( $error_code > 0 )) && error_msg $ERROR_MOVING_TEMP_FILE_NO "$ERROR_MOVING_TEMP_FILE_MSG ("$temp_feed_filename" -> "$feed_filename") (process returned: $error_code)" && exit $ERROR_MOVING_TEMP_FILE_NO
-	echo "Stored feed at $feed_filename"
+	echo "Stored feed at $feed_filename" >&2
 }
 
 function store_youtube_video_titles {
-	jq -r -L ./ 'include "functions"; sort_youtube_rss_by_title | title_format' "$1" > "${2//.txt*/.by_title.txt}" && jq -r -L ./ 'include "functions"; sort_youtube_rss_by_published | title_format' "$1" > "${2//.txt*/.by_date.txt}"
+	output_title_file="${2//.txt*/.by_title.txt}"
+	output_date_file="${2//.txt*/.by_date.txt}"
+	jq -r -L ./ 'include "functions"; sort_youtube_rss_by_title | title_format' "$1" > "$output_title_file" && \
+		echo "Exported video titles from '$1' to '$output_title_file'" >&2 && \
+		jq -r -L ./ 'include "functions"; sort_youtube_rss_by_published | title_format' "$1" > "$output_date_file" && \
+		echo "Exported video titles from '$1' to '$output_date_file'" >&2
 }
 
 function store_recent_youtube_video_titles_for_user {
@@ -175,14 +200,13 @@ function store_recent_youtube_video_titles_for_user {
 	store_youtube_video_titles "$input" "$output"
 	error_code=$?
 	(( $error_code > 0 )) && error_msg $ERROR_STORING_YOUTUBE_VIDEO_TITLES_NO "$ERROR_STORING_YOUTUBE_VIDEO_TITLES_MSG '$input' > '$output') (process returned: $error_code)" && exit $ERROR_STORING_YOUTUBE_VIDEO_TITLES_NO
-	echo "Exported video titles from '$input' to '$output'"
 
 	input="$(youtube_new_videos_feed_file_for_user "$youtube_username")"
 	output="$(youtube_new_videos_titles_file_for_user "$youtube_username")"
 	store_youtube_video_titles "$input" "$output"
 	error_code=$?
 	(( $error_code > 0 )) && error_msg $ERROR_STORING_YOUTUBE_VIDEO_TITLES_NO "$ERROR_STORING_YOUTUBE_VIDEO_TITLES_MSG '$input' > '$output') (process returned: $error_code)" && exit $ERROR_STORING_YOUTUBE_VIDEO_TITLES_NO
-	echo "Exported video titles from '$input' to '$output'"
+	echo "Exported video titles from '$input' to '$output'" >&2
 }
 
 
@@ -190,6 +214,9 @@ function merge_json_files {
 	jq -s '.[0] * .[1]' "$1" "$2"
 }
 
+function merge_youtube_feed_files {
+	jq -s '.[0].items += .[1].items | .[0] | .items |= unique_by(.link)' "$1" "$2"
+}
 #sort_by_title
 #jq -s '.[0] * .[1]' kikoskia.rss.*.json  | jq -r '.items|sort_by(.title)[] | "\(.link) # 📅\(.publishedParsed) - \(.author.name) - \(.title)"'
 # MPV_SELECTION="$(cat kikoskia.rss.merged.20210109.json | jq -r '.items|sort_by(.title)[] | "\(.link) # 📅\(.publishedParsed) - \(.author.name) - \(.title)"' | fzf --no-sort --tac)" && pv ${MPV_SELECTION%% *}
--- youtube.sh
+++ youtube.sh
@@ -1,16 +1,42 @@
 #!/usr/bin/env bash
 # (C) 2021, Filip H.F. "FiXato" Slagter, contact.fixato.org
 set -o nounset -o pipefail
-username=${1:-""}
+cmd="${1:-""}"
 
 . "$(dirname "$(readlink -f "$0")")/functions.sh"
 
-if [ "$username" != "" ]; then
-	store_recent_youtube_video_titles_for_user "$username"
-else
-	videos_file=""$PLAY_VIDEO_YOUTUBE_DIR"/$(find "$PLAY_VIDEO_YOUTUBE_DIR" -type f -name '*.txt' | grep -oP "$PLAY_VIDEO_YOUTUBE_DIR"'/\K(.+)' | sort -u | fzf --preview "cat \"$PLAY_VIDEO_YOUTUBE_DIR/\"{}" --preview-window=down --no-sort --tac -q "'videos 'by_title.txt !^si")"
-	error_code=$?
-	(( $error_code > 0 )) && error_msg $error_code "Error while selecting file" && exit $error_code
-	PLAY_VIDEO_HISTORY_FILE="$videos_file" play_video && "$0"
+case "$cmd" in
+	"update_feed_from_fzf")
+		feed_username="$(echo "${2}" | grep -oP "$PLAY_VIDEO_YOUTUBE_DIR"'/\K[^/]+')"
+		echo "Feed username: $feed_username" >> test.log && "$0" "update_feed" "$feed_username"
+		;;
 
-fi
+	"update_feed")
+		username="$(ensure_param 2 "youtube_username" "$@")" || exit $?
+		channel_id="${3:-""}"
+
+		if [ "$channel_id" != "" ]; then
+			store_channel_id_for_user "$username" "$channel_id"
+		fi
+		store_recent_youtube_video_titles_for_user "$username"
+		;;
+
+	"list")
+		find "$PLAY_VIDEO_YOUTUBE_DIR" -type f -name '*.txt' | sort -u #| grep -oP "$PLAY_VIDEO_YOUTUBE_DIR"'/\K(.+)' | sort -u
+		;;
+
+	"")
+		#FZF_DEFAULT_COMMAND="\"$(script_real_path)\" list" videos_file="$("$0" "list" | fzf --preview "cat {}" --preview-window=down --with-nth=-2,-1 --delimiter=/ --bind "ctrl-u:reload(tmux display-message updating && \"$(script_real_path)\" update_feed_from_fzf {} && $FZF_DEFAULT_COMMAND)" --no-sort --tac --query "'videos 'by_title.txt !^si")"
+
+		videos_file="$(FZF_DEFAULT_COMMAND="$0"' list' fzf --preview "cat {}" --preview-window=down --with-nth=-2,-1 --delimiter=/ --bind 'ctrl-u:reload(tmux display-message updating && ./youtube.sh update_feed_from_fzf {} && $FZF_DEFAULT_COMMAND),ctrl-n:execute(read -p "Username? " username && read -p "Channel ID? " channel_id && ./youtube.sh update_feed "$username" "$channel_id")+reload($FZF_DEFAULT_COMMAND),ctrl-r:reload($FZF_DEFAULT_COMMAND)' --no-sort --tac --query "'videos '.by_title !^si")"
+
+		error_code=$?
+		(( $error_code > 0 )) && error_msg $error_code "Error while selecting file" && exit $error_code
+		PLAY_VIDEO_HISTORY_FILE="${videos_file}" play_video && "$0"
+		;;
+
+	*)
+		echo "Unknown command: '$cmd'"
+		;;
+
+esac
